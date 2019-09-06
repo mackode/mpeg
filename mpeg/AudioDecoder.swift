be6a5eb0109354ca89dc6b37747f1bc1e04b8523
Bartłomiej Makowski <biuro@mackode.pl>
2019-09-06 22:50:14 +0200

    Basic framework structure

---
+++ mpeg/AudioDecoder.swift
@@ -0,0 +1,9 @@
+//
+//  AudioDecoder.swift
+//  mpeg
+//
+//  Created by Mackode - Bartlomiej Makowski on 06/09/2019.
+//  Copyright © 2019 pl.mackode. All rights reserved.
+//
+
+import Foundation
---
+++ mpeg/Demuxer.swift
@@ -0,0 +1,9 @@
+//
+//  Demuxer.swift
+//  mpeg
+//
+//  Created by Mackode - Bartlomiej Makowski on 06/09/2019.
+//  Copyright © 2019 pl.mackode. All rights reserved.
+//
+
+import Foundation
